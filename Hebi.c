2d4bd15f4d9454eaf58d25bc8257ad2a806304c3
deba <debabrata1729@gmail.com>
2023-06-05 23:02:18 +0530

    balldrop-animate succeessfulgit add Hebi.c!

--- Hebi.c
+++ Hebi.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<unistd.h>
 
 int i,j, height=30,width=60;
-int x,y;
+int x=10,y=10;
 
-void circle(int x, int y,int i, int j)
+int circle(int x, int y,int i, int j)
 {
-    if(i==x&&j==y) printf("o");
-    else printf(" ");
+    if(i==x&&j==y) return 1;
+    else return 0;
 }
 
 void drawboard()
@@ -17,19 +18,26 @@ void drawboard()
         for(int j=0;j<width;j++)
         {
             if(i==0||i==height-1||j==0||j==width-1) printf("#");
-            else{
-                circle(4,10,i,j);
-            }
+            else if(circle(x,y,i,j)) printf("O");
+            else printf(" ");
         }
         printf("\n");
     }
 }
 
+//now i just need to update x and y values and call the drawboard func
 
 
 int main()
 {
-    drawboard();
+    //moving right
+
     
+    for(int i=0;i<30;i++){
+        drawboard();
+        x++;
+        sleep(1);
+        system("clear");
+    }
     return 0;
 }
