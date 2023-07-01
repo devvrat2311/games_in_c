3effe9ba2c4a643ceaa5881feea3a84bc4c3e5c1
Debabrata Adhikari <debabrataadhikari@pop-os.localdomain>
2023-07-01 23:07:50 +0530

    added borders

--- new12.cpp
+++ new12.cpp
@@ -1,12 +1,26 @@
 #include<iostream>
 #include "kbhit.hpp"
 #include "matrix.hpp"
+#include<cstdlib>
 
 using namespace std;
 
-Matrix grid(30,60);
+Matrix grid(45,60);
 
 int yCoord = 14, xCoord = 29;
+void borderMake()
+{
+	for(int i=0; i<45; i++)
+	{
+		for(int j=0; j<60; j++)
+		{
+			if(i==0 || i==44 || j==0 || j==59)
+			{
+				grid.insert(i,j,-1);
+			}
+		}
+	}
+}
 
 void move()
 {
@@ -35,11 +49,15 @@ void move()
 			
 			if (key == 27) 
 				break;
-			grid.insert(yCoord, xCoord, 1);	
-			grid.display();
+			grid.insert(yCoord, xCoord, 1);
+			system("clear");	
+	  		borderMake();
+			grid.display(true);
+
 		}
 	}
-}	
+}
+
 int main()
 {
 	grid.insert(yCoord, xCoord, 1);
